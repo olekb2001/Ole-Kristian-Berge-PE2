4f9b044802e3ee06b8082a54af17d8d41fb0152d
olekb2001 <olekb2001@gmail.com>
2026-01-20 22:40:07 +0100

    added fonts and started on layout jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,27 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout(){
+    return(
+        <>
+        {/*navbar for later*/}
+        <div>Navbar fir later</div>
+        
+        
+        
+        
+        
+        </>
+
+
+
+
+
+
+    )
+
+
+
+
+
+
+}
