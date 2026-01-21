1f155c63ad20da687ed3267081854aacac88815d
olekb2001 <olekb2001@gmail.com>
2026-01-21 14:57:32 +0100

    fixed code structure and added dropdown to match figma prototype

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,62 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/Holidaze-logo-desktop.png"
+import logo from "../assets/Holidaze-logo-desktop.png";
 import "./Navbar.css";
+import { useState } from "react";
 
+export default function Navbar({ isLoggedIn, role }) {
+  const [isAvatarMenuOpen, setIsAvatarMenuOpen] = useState(false);
 
-export default function Navbar({isLoggedIn, role}){
-    return(
-        <nav className ="navbar">
-            <div className="navbar-left">
-                <Link to = "/">
-                    <img src = {logo} alt="Holidaze logo" className="navbar-logo"/>
-                </Link>
-            </div>
-            <div className="navbar-right">
-                <Link to="/">Home</Link>
-                <Link to="/venues">Venues</Link>
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/">
+          <img
+            src={logo}
+            alt="Holidaze logo"
+            className="navbar-logo"
+          />
+        </Link>
+      </div>
 
-                {!isLoggedIn && <Link to="/login" className="login-button">Login</Link>}
+      <div className="navbar-right">
+        <Link to="/">Home</Link>
+        <Link to="/venues">Venues</Link>
 
-                {isLoggedIn && (
-                    <button className ="avatar-button">
-                        * {/* placeholder for future improvement*/}
-                    </button>
+        {!isLoggedIn && (
+          <Link to="/login" className="login-button">
+            Login
+          </Link>
+        )}
+
+        {isLoggedIn && (
+          <div className="avatar-container">
+            <button
+              className="avatar-button"
+              onClick={() =>
+                setIsAvatarMenuOpen(!isAvatarMenuOpen)
+              }
+              aria-label="User menu"
+            >
+              *
+            </button>
+
+            {isAvatarMenuOpen && (
+              <div className="avatar-dropdown">
+                {role === "customer" && (
+                  <Link to="/bookings">Bookings</Link>
                 )}
-            </div>
-        </nav>
-    );
+
+                {role === "manager" && (
+                  <Link to="/my-venues">My Venues</Link>
+                )}
+
+                <button type="button">Update Avatar</button>
+                <button type="button">Logout</button>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
