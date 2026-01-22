347e6aa91b8569a4cde57b5a52671ffffbe00ff5
olekb2001 <olekb2001@gmail.com>
2026-01-22 14:00:27 +0100

    psudo code

--- src/pages/VenueDetails.jsx
+++ src/pages/VenueDetails.jsx
@@ -1,12 +1,9 @@
 // first find the venue id from the url of the page
 // then i need to create a state where i can store the venue 
-// when pag loads , then i get spesific venue by that id
-
-
-
-
-
-
+// when page loads , then i get spesific venue by that id
+// then i want to save the result in state
+// if the venue has not leaded, i want to show a loading message on screen
+// when loading is finished then i want to display the name and desc of the venue
 
 export default function VenueDetails() {
   return (
