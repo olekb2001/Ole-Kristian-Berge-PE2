10d31ef35b948abea97c118443160a34acc5e1b1
olekb2001 <olekb2001@gmail.com>
2026-01-22 13:24:51 +0100

    added contents to the cards, next update make book now button go to VenueDetails

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -3,11 +3,13 @@ import "./VenueCard.css";
 export default function VenueCard({venue}){
     return(
         <div className="venue-card-container">
-            <div className="venue-image"></div>
+            <div className="venue-image">
+                {venue.media?.[0] && (<img src = {venue.media[0]} alt = {venue.name}/>)}
+            </div>
             <div className="venue-info">
-                <h3 className="venue-title">Venue Name</h3>
-                <p className="venue-location">City, Country</p>
-                <p className="venue-price">$120 / night</p>
+                <h3 className="venue-title">{venue.name}</h3>
+                <p className="venue-location">{venue.location?.city}, {venue.location?.country}</p>
+                <p className="venue-price">{venue.price} / night</p>
                 <button className="venue-button">Book Now</button>
             </div>
         </div>
