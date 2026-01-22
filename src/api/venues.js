d9f120aa85ebc0a8c93811b3ad14d322eb208f3a
olekb2001 <olekb2001@gmail.com>
2026-01-22 12:57:41 +0100

    forgot () behind json

--- src/api/venues.js
+++ src/api/venues.js
@@ -1,11 +1,11 @@
 export async function getVenues() {
     try{
         const response = await fetch("https://api.noroff.dev/api/v1/holidaze/venues");
-        const data = await response.json;
+        const data = await response.json();
         return data;
     }
     catch(error){
         console.error("not found the venues", error);
-        return [];
+        return[];
     }
 }
