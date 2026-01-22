10e7d61ec6ec5e38c777df12119426e3c65d3cbe
olekb2001 <olekb2001@gmail.com>
2026-01-22 10:44:33 +0100

    created venuecard.jsx and venuecard css

---
+++ src/components/VenueCard.jsx
@@ -0,0 +1,30 @@
+import "./VenueCard.css";
+
+export default function VenueCard(){
+    return(
+        <div className="venue-card-container">
+            <div className="venue-image"></div>
+
+            <div className="venue-info">
+                <h3 className="venue-title"></h3>
+
+
+
+
+            </div>
+            
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+    )
+}
