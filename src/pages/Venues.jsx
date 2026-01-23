fd1b2f9ece1f3355ece2733e3abfd29c3940596d
olekb2001 <olekb2001@gmail.com>
2026-01-23 17:23:01 +0100

    added comments in the venues.jsx file

--- src/pages/Venues.jsx
+++ src/pages/Venues.jsx
@@ -1,12 +1,21 @@
+/*     
+this page displayes all of the venues that are from the api.
+
+when the component has mounted, it fetches the venues and store them in "state".
+then the venues are displyed using the venuecard component.
+ */
 import VenueCard from "../components/VenueCard";
 import "./Venues.css"
 import { useEffect, useState } from "react";
 import { getVenues } from "../api/venues";
 
 export default function Venues() {
+  //this holds the list of venues tht is fetched from the api
   const [venues, setVenues] = useState([]);
 
   useEffect(() =>{
+    //runs once when the page loads 
+    // fetches venues and saves them into state 
     async function loadTheVenues() {
       const data = await getVenues();
       setVenues(data);
@@ -29,6 +38,7 @@ export default function Venues() {
       <h2 className="all-venues-title">All Venues</h2>
 
       <div className="venues-grid">
+        {/* loops through the venues and shows just one card per venue*/}
         {venues?.map((venue) => (
           <VenueCard key ={venue.id} venue = {venue}/>
         ))}
