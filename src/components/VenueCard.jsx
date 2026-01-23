562fe506e02d1ef180172a6521a718836cf03e30
olekb2001 <olekb2001@gmail.com>
2026-01-23 17:38:55 +0100

    added comments in the venuecard.jsx file

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -1,3 +1,8 @@
+/*    
+this is the reasuable component that displays the venue card
+
+it shows image, name, location, price , and link to the venue detail page
+*/
 import "./VenueCard.css";
 import { Link } from "react-router-dom";
 
@@ -5,12 +10,18 @@ export default function VenueCard({venue}){
     return(
         <div className="venue-card-container">
             <div className="venue-image">
+                {/* The noroff api uses media objects so we are acessing it through media[0].url.
+                optional chaining (?.) is used because not all
+                venues have images. This prevents the app from crashing.
+                */}
                 {venue.media?.[0]?.url && (<img src = {venue.media[0].url} alt = {venue.name}/>)}
             </div>
             <div className="venue-info">
                 <h3 className="venue-title">{venue.name}</h3>
+                {/* location might not exist so its better to be on the safe side and add optional chaining so it does not crash */}
                 <p className="venue-location">{venue.location?.city}, {venue.location?.country}</p>
                 <p className="venue-price">{venue.price} / night</p>
+                {/* this navigates to the details page by using the venues id*/}
                 <Link to={`/venues/${venue.id}`} className="venue-button">Book Now</Link>
             </div>
         </div>
