e46850c8eafa1ce640cf310335796714bc708509
olekb2001 <olekb2001@gmail.com>
2026-01-23 18:05:20 +0100

    commented on the navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
+/*
+navbar changes whether you are logged in, logged out and depending 
+on which role you have
+
+if you are logged out it shows a login button.
+if you are logged in it shows a avatar menu with options depending on your role
+*/
 import { Link } from "react-router-dom";
 import logo from "../assets/Holidaze-logo-desktop.png";
 import "./Navbar.css";
 import { useState } from "react";
 
 export default function Navbar({ isLoggedIn, role }) {
+    //this controls the dropdown visibility
   const [isAvatarMenuOpen, setIsAvatarMenuOpen] = useState(false);
 
   return (
@@ -22,14 +30,17 @@ export default function Navbar({ isLoggedIn, role }) {
         <Link to="/">Home</Link>
         <Link to="/venues">Venues</Link>
 
+        {/* if user is not logged in show login button */}
         {!isLoggedIn && (
           <Link to="/login" className="login-button">
             Login
           </Link>
         )}
 
+        {/*  if the user is logged in, show the avatar menu*/}
         {isLoggedIn && (
           <div className="avatar-container">
+            {/* clicking it toggles the dropdown menu*/}
             <button
               className="avatar-button"
               onClick={() =>
@@ -42,6 +53,8 @@ export default function Navbar({ isLoggedIn, role }) {
 
             {isAvatarMenuOpen && (
               <div className="avatar-dropdown">
+
+                {/* menu items based on yor role*/}
                 {role === "customer" && (
                     <div className="dropdown-item">
                         <Link to="/bookings">Bookings</Link>
