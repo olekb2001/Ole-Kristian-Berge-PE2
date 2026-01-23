c2b70b0f2d7eca9c476256b8895a2041821cad03
olekb2001 <olekb2001@gmail.com>
2026-01-23 17:07:28 +0100

    added commenting in the venues.js file

--- src/api/venues.js
+++ src/api/venues.js
@@ -1,5 +1,16 @@
+/*               
+This file contains all api calls related to venues.
+
+instead of having to call fetch inside the components, i have stored the api logic 
+in its seperate file. this makes it so components is cleaner and 
+makes it easier to maintain and and reuse on different pages.
+*/
+
+// this is the url for holidaze api v2
 const API_URL = "https://v2.api.noroff.dev/holidaze";
 
+// here i fetch all the venues fromm the api
+// the api v2 returns data inside { data: ... } so im accesing it through json.data
 export async function getVenues() {
     try{
         const response = await fetch(`${API_URL}/venues`);
@@ -12,8 +23,8 @@ export async function getVenues() {
     }
 }
 
-
-
+// im fetching a single venue by the id from the url.
+// this is used on the VenueDetails Page.
 export async function findVenueId(id) {
     try{
         const response = await fetch(`${API_URL}/venues/${id}`);
