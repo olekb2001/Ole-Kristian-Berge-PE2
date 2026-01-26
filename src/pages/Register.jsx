76dbd796c4a79abf70bfb1d0ac728b5524b5a3c8
olekb2001 <olekb2001@gmail.com>
2026-01-26 13:36:33 +0100

    progression commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,5 +12,56 @@ export default function Register(){
     const [message, setMessage] = useState("");
 
     // this runs when the form is submitted
+    async function fixSubmit(event){
+        event.preventDefault();
+
+        try{
+            await registerTheUser(name, email, password, venueManager);
+            setMessage("User registered successfully. You can now log in.")
+        }
+        catch(error){
+            setMessage("Something went wrong. Please try again");
+        }
+    }
+    return(
+        <div className="register-page">
+            <h1 className="register-title">Register</h1>
+            <p className="register-subtitle">Create an account to book venues or manage your own</p>
+
+            <div className="register-card">
+                <form onSubmit={fixSubmit} className="register-form">
+                    <label>Name</label>
+                    <input
+                        type="text"
+                        placeholder="Your full name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+
+                    <label>Name</label>
+                    <input
+                        type="text"
+                        placeholder="Your full name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+
+                
+
+
+
+
+
+                </form>
+
+
+
+            </div>
+
+
+
+
+        </div>
+    )
     
 }
