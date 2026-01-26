5b3a719df9c72dd435487d1f04a7aaefbed2800c
olekb2001 <olekb2001@gmail.com>
2026-01-26 20:27:06 +0100

    login page started

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+import { loginTheUser } from "../api/auth";
+import { Link, useNavigate } from "react-router-dom";
+import "./Register.css"
+
+/*
+
+*/
+
 export default function Login() {
   return <h1>Login Page</h1>;
 }
