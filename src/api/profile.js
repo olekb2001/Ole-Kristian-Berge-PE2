0d53cf51cc4fb38c0d34a9dc300b1c83add24f1a
olekb2001 <olekb2001@gmail.com>
2026-01-29 20:33:21 +0100

    chore commit

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,1 +1,13 @@
 const API_URL = "https://v2.api.noroff.dev/holidaze";
+
+export async function getMyBookings(){
+    // get logged in user from locStorage
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if(!user){
+        throw new error("You must be logged in");
+    }
+
+    const accessToken = user.accessToken;
+    const name = user.name;
+}
