512585c365c6f832d991cfcb591ada143d4f4150
olekb2001 <olekb2001@gmail.com>
2026-01-30 01:17:59 +0100

    bookings page done and divided content into divs for styling purpose to match figma

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -25,21 +25,38 @@ export default function Bookings() {
   return (
     <div>
       <h1>My Bookings</h1>
+      <p>Here are your upcoming stays</p>
       {bookings.map((booking) => {
         const from = new Date(booking.dateFrom).toLocaleDateString();
         const to = new Date(booking.dateTo).toLocaleDateString();
 
         return (
-          <div key={booking.id}>
-            <h2>{booking.venue?.name}</h2>
-            <p>
-              {booking.venue?.location?.city},{" "}
-              {booking.venue?.location?.country}
-            </p>
-            <p>
-              {from} - {to}
-            </p>
-            <p>${booking.venue?.price} / night</p>
+          <div key={booking.id} className="booking-card">
+            {/*left img*/}
+            <div className="booking-image">
+              {booking.venue?.media?.[0]?.url && (
+                <img
+                  src={booking.venue.media[0].url}
+                  alt={booking.venue.name}
+                />
+              )}
+            </div>
+            {/*middle - info*/}
+            <div className="booking-info">
+              <h2>{booking.venue?.name}</h2>
+              <p>
+                {booking.venue?.location?.city},{" "}
+                {booking.venue?.location?.country}
+              </p>
+              <p>
+                {from} - {to}
+              </p>
+            </div>
+            {/* price + status*/}
+            <div className="booking-meta">
+              <p className="booking-price">${booking.venue?.price} / night</p>
+              <p className="booking-status">Upcoming</p>
+            </div>
           </div>
         );
       })}
