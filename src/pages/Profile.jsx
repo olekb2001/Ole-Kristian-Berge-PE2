7ac5c01def8cdfa616b456bfe6e60a29f742e0ea
olekb2001 <olekb2001@gmail.com>
2026-01-31 19:30:58 +0100

    structure almost finished startting styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,29 +1,26 @@
-
-
-
-
-
 export default function Profile() {
   const user = JSON.parse(localStorage.getItem("user"));
 
-  if(!user){
-    return <p>You must be logged in</p>
+  if (!user) {
+    return <p>You must be logged in</p>;
   }
 
-  return(
-    <div className="">
+  return (
+    <div className="profile-page">
+      <div className="profile-header">
+        <h1 className="profile-title">My Profile</h1>
+        <p className="profile-undertitle">Make profile changes etc</p>
+      </div>
+      <div className="profile-card">
+        {user.avatar && (
+          <img src={user.avatar} alt={user.name} className="profile-avatar" />
+        )}
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <p>Role: {user.venueManager ? "Venue Manager" : "Customer"}</p>
 
-
-
-
-
+        <button>Update Avatar</button>
+      </div>
     </div>
-
-
-
-
-
-  )
-  
-  
+  );
 }
