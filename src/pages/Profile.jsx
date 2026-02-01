d703f0fa7ae1c987179416c21af08f5d434f1624
olekb2001 <olekb2001@gmail.com>
2026-02-01 01:36:52 +0100

    commenting on code

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,21 +2,34 @@ import "./Profile.css";
 import { useState } from "react";
 
 export default function Profile() {
+  // get thje logged in user from locStorage
   const user = JSON.parse(localStorage.getItem("user"));
+  
+  // this is used if the avatar url is broken
+  // so that we can show a fallback instead of the broken image icon
   const [avatarError, setAvatarError] = useState(false);
 
+  // if no user is found, block access to the page
   if (!user) {
     return <p>You must be logged in</p>;
   }
 
   return (
     <div className="profile-page">
+          {/* page header*/}
       <div className="profile-top">
         <h1 className="profile-title">My Profile</h1>
         <p className="profile-subtitle">Manage your profile information</p>
       </div>
+       {/* main profile card*/}
       <div className="profile-card">
+        {/* avatar section*/}
         <div className="profile-avatar-wrapper">
+          {/*
+            If the user has an avatar and it loads correctly,
+            show the image. If it fails to load, show a placeholder
+            with the first letter of the users name.
+          */}
           {user.avatar && !avatarError ? (
             <img
               src={user.avatar}
@@ -30,11 +43,12 @@ export default function Profile() {
             </span>
           )}
         </div>
+        {/* user information*/}
         <div className="profile-info">
           <h2>{user.name}</h2>
           <p>{user.email}</p>
           <p>Role: {user.venueManager ? "Venue Manager" : "Customer"}</p>
-
+           {/* this button will later allow the user to update their avatar*/}
           <button className="profile-button">Update Avatar</button>
         </div>
       </div>
