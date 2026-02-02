882f17d21a0912954573ffdc33ab3665a1695bbd
olekb2001 <olekb2001@gmail.com>
2026-02-02 07:09:16 +0100

    chore commit

--- src/pages/CreateVenue.jsx
+++ src/pages/CreateVenue.jsx
@@ -4,7 +4,38 @@ this page lets venue managers create a new venue.
 here the manager fills out a form with venue details
 which will later be sent to the api to create the venue.
 */
-import "./CreateVenue.css"
-export default function CreateVenue(){
-    return <h2>hello worls </h2>
+import "./CreateVenue.css";
+export default function CreateVenue() {
+  return (
+    <div className="create-venue-page">
+      <div className="create-venue-top">
+        <h1 className="create-venue-title">Create New Venue</h1>
+        <p className="create-venue-subtitle">
+          Fill in the details below to publish your venue
+        </p>
+      </div>
+
+      <div className="create-venue-layout">
+        {/* left container*/}
+        <div className="create-venue-left">
+          <label>Venue Name</label>
+          <input type="text" />
+
+          <label>Description</label>
+          <textarea />
+
+          <div className="two-inputs">
+            <div>
+              <label>Pricing Per Night</label>
+              <input type="number" />
+            </div>
+            <div>
+              <label>Maximum Guests</label>
+              <input type="number"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
