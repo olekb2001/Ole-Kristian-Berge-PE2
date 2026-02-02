9bb1631f819f931282ffd94c94c02f4c8419825f
olekb2001 <olekb2001@gmail.com>
2026-02-02 06:14:12 +0100

    chore commit

--- src/pages/MyVenues.jsx
+++ src/pages/MyVenues.jsx
@@ -1,3 +1,13 @@
+import { useEffect, useState } from "react";
+import { getMyVenues } from "../api/profile";
+import { Link } from "react-router-dom";
+import "./MyVenues.css";
+
+
+
+
+
+
 export default function MyVenues() {
-  return <h1>My Venues</h1>;
+  
 }
