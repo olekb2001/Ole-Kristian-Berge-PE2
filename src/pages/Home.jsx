67b7f4f41b91c17257ba26486a9ca6c3ad9b1922
olekb2001 <olekb2001@gmail.com>
2026-02-04 17:27:03 +0100

    made a very simple home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,38 @@
 import { useEffect, useState } from "react";
 import { getVenues } from "../api/venues";
 import VenueCard from "../components/VenueCard";
+import "./Home.css"
 
-export default function Home(){
+export default function Home() {
   const [venues, setVenues] = useState([]);
 
-  //fetch venues when the page loads 
+  //fetch venues when the page loads
   useEffect(() => {
-    async function loadVenues(){
+    async function loadVenues() {
       const data = await getVenues();
-      setVenues(data.slice(0, 3)); // here i only show the first 3 
+      setVenues(data.slice(0, 3)); // here i only show the first 3
     }
     loadVenues();
   }, []);
 
-  return(
-    <div className="venues-page">
+  return (
+    <div className="home-page">
       {/*  top sect */}
-      <h1 className="venues-title">Find Your Perfect Holiday Stay</h1>
-      <p className="venues-subtitle">Browse unique venues and book your next getaway</p>
+      <div className="home-hero">
+        <h1 className="venues-title">Find Your Perfect Holiday Stay</h1>
+        <p className="venues-subtitle">
+          Browse unique venues and book your next getaway
+        </p>
+      </div>
 
+      {/* popular venues*/}
+      <h2 className="all-venues-title popular-title">Popular venues</h2>
 
-
-
+      <div className="venues-grid">
+        {venues.map((venue) => (
+          <VenueCard key={venue.id} venue={venue} />
+        ))}
+      </div>
     </div>
-
-
-
-
-  )
+  );
 }
