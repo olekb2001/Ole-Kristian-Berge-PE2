078f0f0c103c3088238ab2afd1c2f101ca6b59aa
olekb2001 <olekb2001@gmail.com>
2026-02-04 17:40:16 +0100

    fixed wrong structure and and wrongly named function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,33 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/Holidaze-logo-desktop.png";
-import "./footer.css";
+import "./Footer.css";
 
-export default function footer() {
+export default function Footer() {
   const user = JSON.parse(localStorage.getItem("user"));
 
   const isLoggedIn = !!user;
   const role = user?.venueManager ? "manager" : "customer";
-}
 
-return(
+  return (
     <footer className="footer">
-        <div className="footer-left">
-            <img src={logo} alt="Holidaze Logo" className="footer-logo"/>
-        </div>
-        <div className="footer-links">
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
+      <div className="footer-left">
+        <img src={logo} alt="Holidaze Logo" className="footer-logo" />
+      </div>
+      <div className="footer-links">
+        <Link to="/">Home</Link>
+        <Link to="/venues">Venues</Link>
 
+        {isLoggedIn && role === "customer" && (
+          <Link to="/bookings">Bookings</Link>
+        )}
 
-        </div>
-
-
-
-
+        {isLoggedIn && role === "manager" && (
+          <Link to="/my-venues">My Venues</Link>
+        )}
+      </div>
+      <div className="footer-copy">
+        © 2026 Holidaze – Created by Ole Kristian Berge
+      </div>
     </footer>
-)
+  );
+}
